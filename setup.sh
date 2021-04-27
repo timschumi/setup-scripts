920673cdd4f47b5a8a431c68ac2f6cdabe7f62fd
Tim Schumacher <timschumi@gmx.de>
2021-04-27 15:53:36 +0200

    Install a theme and icon pack

--- setup.sh
+++ setup.sh
@@ -3,8 +3,22 @@
 OS_INSTALL_LIGHTDM=1
 OS_INSTALL_XFCE=1
 OS_INSTALL_NETWORKMANAGER=1
+OS_THEME="adapta-gtk-theme:Adapta-Nokto-Eta"
+OS_ICONS="papirus-icon-theme:Papirus"
 
 _OS_NEEDS_XORG="${OS_INSTALL_LIGHTDM}"
+
+if [ -n "${OS_THEME}" ]; then
+_OS_THEME_SPLIT=(${OS_THEME//:/ })
+_OS_THEME_PACKAGE="${_OS_THEME_SPLIT[0]}"
+_OS_THEME_NAME="${_OS_THEME_SPLIT[1]}"
+fi  # OS_THEME
+
+if [ -n "${OS_ICONS}" ]; then
+_OS_ICONS_SPLIT=(${OS_ICONS//:/ })
+_OS_ICONS_PACKAGE="${_OS_ICONS_SPLIT[0]}"
+_OS_ICONS_NAME="${_OS_ICONS_SPLIT[1]}"
+fi  # OS_ICONS
 
 >&2 echo "--- Disabling sudo password requirement ---"
 echo "%wheel ALL=(ALL) NOPASSWD: ALL" | sudo tee /etc/sudoers.d/15-nopasswd
@@ -37,6 +51,27 @@ if [ -n "${OS_INSTALL_NETWORKMANAGER}" ]; then
 sudo pacman -S networkmanager network-manager-applet --noconfirm
 sudo systemctl enable NetworkManager
 fi  # OS_INSTALL_NETWORKMANAGER
+
+
+if [ -n "${OS_THEME}" ]; then
+>&2 echo "--- Installing Theme ---"
+sudo pacman -S "${_OS_THEME_PACKAGE}" --noconfirm
+
+if [ -n "${OS_INSTALL_XFCE}" ]; then
+xfconf-query -c xsettings -p /Net/ThemeName -s "${_OS_THEME_NAME}"
+xfconf-query -c xfce4-notifyd -p /theme -s "${_OS_THEME_NAME}"
+fi  # OS_INSTALL_XFCE
+fi  # OS_THEME
+
+
+if [ -n "${OS_ICONS}" ]; then
+>&2 echo "--- Installing Icons ---"
+sudo pacman -S "${_OS_ICONS_PACKAGE}" --noconfirm
+
+if [ -n "${OS_INSTALL_XFCE}" ]; then
+xfconf-query -c xsettings -p /Net/IconThemeName -s "${_OS_ICONS_NAME}"
+fi  # OS_INSTALL_XFCE
+fi  # OS_ICONS
 
 
 >&2 echo "--- Enabling sudo password requirement ---"
